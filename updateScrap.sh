f00e20f01253c0e006e6fa2c5928ae3a950a976b
xandjiji <xandjiji@gmail.com>
2021-02-10 11:36:19 -0300

    automation scripts change

--- updateScrap.sh
+++ updateScrap.sh
@@ -5,5 +5,6 @@ npm run update
 
 mv -v ~/tibia-bazaar-scraper/LatestCharacterData.json ~/exevo-pan/src/
 mv -v ~/tibia-bazaar-scraper/serverData.json ~/exevo-pan/src/
+mv -v ~/tibia-bazaar-scraper/ItemsData.json ~/exevo-pan/src/
 
 sh ~/deploy.sh
