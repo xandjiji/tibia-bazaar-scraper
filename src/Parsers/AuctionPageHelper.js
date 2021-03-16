e7bfa4470d682f389b58dc24d3bbf8cf389566f9
xandjiji <xandjiji@gmail.com>
2021-03-16 20:26:24 -0300

    id()

--- src/Parsers/AuctionPageHelper.js
+++ src/Parsers/AuctionPageHelper.js
@@ -4,7 +4,7 @@ class AuctionPageHelper {
     }
 
     fetchError() {
-        
+        /* IMPLEMENT */
     }
 
     errorCheck() {
@@ -16,6 +16,28 @@ class AuctionPageHelper {
             return false;
         }
     }
+
+    isFinished() {
+        const bidElement = this.$('.MyMaxBidLabel')[0];
+        if (bidElement) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    id() {
+        const buttonElement = this.$('.BigButtonText')[1];
+        const splittedString = buttonElement.attribs.onclick.split('auctionid=')[1];
+        const id = splittedString.slice(0, splittedString.indexOf('&'));
+        return id;
+    }
+
+    nickname() {
+        return this.$('.Auction .AuctionCharacterName').text();
+    }
+
+
 }
 
 module.exports = AuctionPageHelper;
