18e467c5ce8934d58ea6ea5df6199b7ebfc34c51
xandjiji <xandjiji@gmail.com>
2021-03-20 18:29:00 -0300

    initial ListPageHelper.js + id()

---
+++ src/Parsers/ListPageHelper.js
@@ -0,0 +1,63 @@
+const { dateParsing } = require('../utils');
+
+class ListPageHelper {
+    setHtml($) {
+        this.$ = $;
+    }
+
+    fetchError() {
+        /* IMPLEMENT */
+    }
+
+    errorCheck() {
+        const errorElement = this.$('.Text');
+        const errorText = errorElement[0].children[0].data;
+        if (errorText === 'Error') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    id() {
+        const charNameLink = this.$('.AuctionCharacterName a')[0];
+        const urlObj = new URL(charNameLink.attribs.href);
+        return Number(urlObj.searchParams.get('auctionid'));
+    }
+
+    /* nickname() {
+        return this.$('.Auction .AuctionCharacterName').text();
+    }
+
+    auctionEnd() {
+        const timestampElement = this.$('.AuctionTimer')[0];
+
+        if (timestampElement) {
+            const timestamp = timestampElement.attribs['data-timestamp'];
+            return Number(timestamp);
+        }
+
+        const auctionEndElement = this.$('.ShortAuctionDataValue');
+        const auctionEnd = dateParsing(auctionEndElement[1].children[0].data);
+        return auctionEnd;
+    }
+
+    currentBid() {
+        const currentBidText = this.$('.ShortAuctionDataValue b').text();
+        const currentBid = Number(currentBidText.replace(/,/g, ''));
+        return currentBid;
+    }
+
+    hasBeenBidded() {
+        const hasBeenBiddedElement = this.$('.ShortAuctionDataLabel');
+        const hasBeenBiddedText = hasBeenBiddedElement[2].children[0].data;
+
+        if (hasBeenBiddedText === 'Winning Bid:' || hasBeenBiddedText === 'Current Bid:') {
+            return true;
+        } else {
+            return false;
+        }
+    } */
+}
+
+module.exports = ListPageHelper;
