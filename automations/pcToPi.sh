e7505fd3e40ae875659f336de9b5676a91105ec2
xandjiji <xandjiji@gmail.com>
2021-03-25 21:29:10 -0300

    pcToPi.sh and piToPc.sh

---
+++ automations/pcToPi.sh
@@ -0,0 +1,9 @@
+scp /home/xand/tibia-bazaar-scraper/Output/AllCharacterData.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/bazaarPages.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/ItemsData.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/LatestCharacterData.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/MinifiedCharacterData.json pi:/home/pi/tibia-bazaar-scraper/Output
+
+scp /home/xand/tibia-bazaar-scraper/Output/readableBazaarHistory.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/MinifiedBazaarHistory.json pi:/home/pi/tibia-bazaar-scraper/Output
+scp /home/xand/tibia-bazaar-scraper/Output/scrapHistoryData.json pi:/home/pi/tibia-bazaar-scraper/Output
---
+++ automations/piToPc.sh
@@ -0,0 +1,9 @@
+scp pi:/home/pi/tibia-bazaar-scraper/Output/AllCharacterData.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/bazaarPages.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/ItemsData.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/LatestCharacterData.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/MinifiedCharacterData.json /home/xand/tibia-bazaar-scraper/Output
+
+scp pi:/home/pi/tibia-bazaar-scraper/Output/readableBazaarHistory.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/MinifiedBazaarHistory.json /home/xand/tibia-bazaar-scraper/Output
+scp pi:/home/pi/tibia-bazaar-scraper/Output/scrapHistoryData.json /home/xand/tibia-bazaar-scraper/Output/
