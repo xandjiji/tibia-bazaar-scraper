705464cc99bbc5cef0db19859c8ac0afe701f03b
xandjiji <xandjiji@gmail.com>
2021-03-25 21:31:51 -0300

    extractingAutomations.sh

--- automations/deleteGit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -R -f ~/exevo-pan-deploy
-cd ~/
-git clone --depth=1 --single-branch --branch deploy git@github.com:xandjiji/exevo-pan-deploy.git
---
+++ automations/extractAutomations.sh
@@ -0,0 +1,5 @@
+cp /home/pi/tibia-bazaar-scraper/automations/dailyScrap.sh /home/pi/
+cp /home/pi/tibia-bazaar-scraper/automations/deploy.sh /home/pi/
+cp /home/pi/tibia-bazaar-scraper/automations/pcToPi.sh /home/pi/
+cp /home/pi/tibia-bazaar-scraper/automations/piToPc.sh /home/pi/
+cp /home/pi/tibia-bazaar-scraper/automations/updateScrap.sh /home/pi/
