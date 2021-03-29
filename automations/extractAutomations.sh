31ba085f5b1ef62881134026d7c239d4494fafcd
xandjiji <xandjiji@gmail.com>
2021-03-29 11:05:43 -0300

    extractAutomations.sh improvement

--- automations/extractAutomations.sh
+++ automations/extractAutomations.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-cp /home/pi/tibia-bazaar-scraper/automations/dailyScrap.sh /home/pi/
-cp /home/pi/tibia-bazaar-scraper/automations/deploy.sh /home/pi/
-cp /home/pi/tibia-bazaar-scraper/automations/pcToPi.sh /home/pi/
-cp /home/pi/tibia-bazaar-scraper/automations/piToPc.sh /home/pi/
-cp /home/pi/tibia-bazaar-scraper/automations/updateScrap.sh /home/pi/
-cp /home/pi/tibia-bazaar-scraper/automations/scrapHistory.sh /home/pi/
+cp /home/pi/tibia-bazaar-scraper/automations/*.sh /home/pi/
