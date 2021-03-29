c722952611c7e49d9ecacf8070f382bfa6a14b31
xandjiji <xandjiji@gmail.com>
2021-03-29 11:03:26 -0300

    automation scripts + faster old auctions

---
+++ automations/deployHistory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ~/tibia-bazaar-scraper/Output/PaginatedHistory/*.json ~/exevo-pan-history-data/
+
+cd ~/exevo-pan-history-data/
+git push origin --delete deploy
+git branch -D deploy
+git checkout -b deploy
+
+git add .
+git commit -m "`date`"
+git push --set-upstream origin deploy
+git checkout main
