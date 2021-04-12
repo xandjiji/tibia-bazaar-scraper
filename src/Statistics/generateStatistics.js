90c6b18faa6976f9396352161ff8b83cd107adf8
xandjiji <xandjiji@gmail.com>
2021-04-12 20:10:00 -0300

    total revenue + total negociated

--- src/Statistics/generateStatistics.js
+++ src/Statistics/generateStatistics.js
@@ -11,6 +11,16 @@ const main = async () => {
     const successCount = successAuctions.length;
     const successRate = (successCount / totalItems * 100).toFixed(2);
     console.log(successRate);
+
+    /* total revenue */
+    const totalInitialFeeCoins = data.length * 50;
+    const totalSuccessFee = successAuctions.reduce((total, auction) => total + Math.floor(auction.currentBid * 0.12), 0);
+    const totalRevenue = totalSuccessFee + totalInitialFeeCoins;
+    console.log(totalRevenue);
+
+    /* total negociated */
+    const totalTibiaCoins = successAuctions.reduce((total, auction) => total + auction.currentBid, 0);
+    console.log(totalTibiaCoins);
 }
 
 main();
