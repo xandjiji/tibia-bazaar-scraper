5c9f7b6887d9f11f68f69a236a64622580fbb22c
xandjiji <xandjiji@gmail.com>
2021-04-12 21:06:19 -0300

    vocation percentage

--- src/Statistics/generateStatistics.js
+++ src/Statistics/generateStatistics.js
@@ -47,6 +47,27 @@ const main = async () => {
     const top10Axe = top10BySkillFactory('axe', successAuctions);
     const top10Distance = top10BySkillFactory('distance', successAuctions);
     const top10Shielding = top10BySkillFactory('shielding', successAuctions);
+
+    const vocationCount = {
+        rooker: 0,
+        knight: 0,
+        paladin: 0,
+        sorcerer: 0,
+        druid: 0
+    }
+    const keyArray = Object.keys(vocationCount);
+    for(const char of data) {
+        const vocation = keyArray[char.vocationId];
+        vocationCount[vocation]++
+    }
+
+    const vocationPercentage = {
+        rooker: (vocationCount.rooker / data.length * 100).toFixed(2),
+        knight: (vocationCount.knight / data.length * 100).toFixed(2),
+        paladin: (vocationCount.paladin / data.length * 100).toFixed(2),
+        sorcerer: (vocationCount.sorcerer / data.length * 100).toFixed(2),
+        druid: (vocationCount.druid / data.length * 100).toFixed(2)
+    }
 }
 
 const getTop10ByKey = (key, data) => {
