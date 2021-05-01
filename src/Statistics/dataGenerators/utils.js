bf3f468a3eb73f68b908fd9863890b2f19b2fe5a
xandjiji <xandjiji@gmail.com>
2021-05-01 12:19:56 -0300

    utils.js update

--- src/Statistics/dataGenerators/utils.js
+++ src/Statistics/dataGenerators/utils.js
@@ -5,4 +5,36 @@ const pushAndShift = (item, array) => {
     return newArray;
 }
 
-module.exports = { pushAndShift };
+const getTop10ByKey = (key, data) => {
+    data.sort((a, b) => {
+        return b[key] - a[key];
+    });
+
+    return data.slice(0, 10);
+}
+
+const getTop10BySkillKey = (key, data) => {
+    data.sort((a, b) => {
+        return b.skills[key] - a.skills[key];
+    });
+
+    return data.slice(0, 10);
+}
+
+const top10BySkillFactory = (key, data) => {
+    return getTop10BySkillKey(key, data).map(char => {
+        return {
+            id: char.id,
+            nickname: char.nickname,
+            currentBid: char.currentBid,
+            [key]: char.skills[key]
+        }
+    });
+}
+
+module.exports = {
+    pushAndShift,
+    getTop10ByKey,
+    getTop10BySkillKey,
+    top10BySkillFactory
+};
