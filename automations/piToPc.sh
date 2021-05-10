38752b0a52a6435ab877584340bb76143598e244
xandjiji <xandjiji@gmail.com>
2021-05-10 12:05:02 -0300

    removing history from piToPc.sh

--- automations/piToPc.sh
+++ automations/piToPc.sh
@@ -5,7 +5,3 @@ scp pi:/home/pi/tibia-bazaar-scraper/Output/bazaarPages.json /home/xand/tibia-ba
 scp pi:/home/pi/tibia-bazaar-scraper/Output/ItemsData.json /home/xand/tibia-bazaar-scraper/Output
 scp pi:/home/pi/tibia-bazaar-scraper/Output/LatestCharacterData.json /home/xand/tibia-bazaar-scraper/Output
 scp pi:/home/pi/tibia-bazaar-scraper/Output/MinifiedCharacterData.json /home/xand/tibia-bazaar-scraper/Output
-
-scp pi:/home/pi/tibia-bazaar-scraper/Output/readableBazaarHistory.json /home/xand/tibia-bazaar-scraper/Output
-scp pi:/home/pi/tibia-bazaar-scraper/Output/MinifiedBazaarHistory.json /home/xand/tibia-bazaar-scraper/Output
-scp pi:/home/pi/tibia-bazaar-scraper/Output/scrapHistoryData.json /home/xand/tibia-bazaar-scraper/Output/
