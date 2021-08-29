f6a65a3baec119c2c6b4b05f2903574e26304806
xandjiji <xandjiji@gmail.com>
2021-08-29 20:03:26 -0300

    chore: removing test code

--- src/Guildwar Data/updateGuildMembers.js
+++ src/Guildwar Data/updateGuildMembers.js
@@ -138,7 +138,7 @@ const scrapGuild = async (guildName) => {
 
     let guildMembers = guildHelper.guildMembers()
     guildMembers = guildMembers.filter((member) => member.online)
-    guildMembers = guildMembers.slice(0, 20)
+    /* guildMembers = guildMembers.slice(0, 20) */
     globalIndex = 0;
     globalDataSize = guildMembers.length
 
