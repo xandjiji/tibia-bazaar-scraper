100a49f9f542798fe7cabdb5bbdf17ac6a42efea
xandjiji <xandjiji@gmail.com>
2021-12-03 09:38:17 -0300

    feature(Parsers): PostDataHelper()

---
+++ src/Parsers/PostDataHelper.js
@@ -0,0 +1,44 @@
+class PostDataHelper {
+    types = {
+        items: 0,
+        storeItems: 1,
+        mounts: 2,
+        storeMounts: 3,
+        outfits: 4,
+        storeOutfits: 5
+    }
+
+    setHtml($) {
+        this.$ = $;
+    }
+
+    outfits() {
+        const icons = this.$('.CVIcon')
+
+        const outfits = []
+        icons.each((_, element) => {
+            const title = element.attribs.title
+            const [name, addons] = title.split(' (')
+            const [, addon1, addon2] = addons.split('&')
+            const type = +!!addon1 + (+!!addon2 * 2)
+
+            outfits.push({ name, type })
+        })
+
+        return outfits
+    }
+
+    mounts() {
+        const icons = this.$('.CVIcon')
+
+        const mounts = []
+        icons.each((_, element) => {
+            const name = element.attribs.title
+            mounts.push(name)
+        })
+
+        return mounts
+    }
+}
+
+module.exports = PostDataHelper;
