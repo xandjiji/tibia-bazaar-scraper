807a55ec78b14e77ef1bcdb01184c8cd27668e18
xandjiji <xandjiji@gmail.com>
2021-12-08 06:23:15 -0300

    feature(CharacterData): using previously scraped data

--- src/Character Data/scrapSingle.js
+++ src/Character Data/scrapSingle.js
@@ -7,12 +7,20 @@ const fs = require('fs').promises;
 var globalDataSize;
 var globalIndex = 0;
 var serverListData = {}
-
+var latestData = []
 
 const main = async () => {
     console.log(`${timeStamp('system')} loading bazaarPages.json ...`);
     var data = await fs.readFile('./Output/bazaarPages.json', 'utf-8');
     data = JSON.parse(data);
+
+    console.log(`${timeStamp('system')} Loading 'LatestCharacterData.json'...`);
+    try {
+        var latestData = await fs.readFile('./Output/LatestCharacterData.json', 'utf-8');
+        latestData = JSON.parse(latestData);
+    } catch {
+        latestData = []
+    }
 
     console.log(`${timeStamp('system')} loading ServerData.json ...`);
     serverListData = await fs.readFile('./Output/ServerData.json', 'utf-8');
@@ -45,6 +53,10 @@ const retryWrapper = async (url) => {
 const scrapSinglePage = async (charObject) => {
     const id = charObject.id;
     const nickname = charObject.nickname;
+
+    const previousCharacterData = latestData.find((auction) => auction.id === id)
+    if (previousCharacterData) return previousCharacterData
+
     const $ = await fetchAndLoad(`https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades&page=details&auctionid=${id}&source=overview`);
     globalIndex++;
     console.log(`${timeStamp('neutral')} Scraping ${nickname}'s single page [${globalIndex}/${globalDataSize}]`);
