3bc069dc5fd6c06caee3087c693caa30e6568bca
xandjiji <xandjiji@gmail.com>
2021-12-15 17:44:15 -0300

    feature(Constants): adding CURRENT_AUCTIONS

--- src/Constants/file.ts
+++ src/Constants/file.ts
@@ -7,6 +7,7 @@ const OUTPUT_PATH = './Output'
 
 const names = {
   SERVER_DATA: 'ServerData.json',
+  CURRENT_AUCTIONS: 'CurrentAuctions.json',
 }
 
 const SERVER_DATA = {
@@ -14,6 +15,12 @@ const SERVER_DATA = {
   path: `${OUTPUT_PATH}/${names.SERVER_DATA}`,
 }
 
+const CURRENT_AUCTIONS = {
+  name: names.CURRENT_AUCTIONS,
+  path: `${OUTPUT_PATH}/${names.CURRENT_AUCTIONS}`,
+}
+
 export const file: Record<string, FileConstant> = {
   SERVER_DATA,
+  CURRENT_AUCTIONS,
 }
