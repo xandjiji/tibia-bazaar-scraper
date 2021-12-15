c04e7e3644cc835df05c898ed41b781d9fc1ec2e
xandjiji <xandjiji@gmail.com>
2021-12-15 10:01:33 -0300

    feature(serverData): adding fallback

--- src/Data/serverData/index.ts
+++ src/Data/serverData/index.ts
@@ -9,8 +9,23 @@ const getServerData = async (): Promise<ServerObject[]> => {
     `loading ${logging.coloredText(file.SERVER_DATA.name, 'highlight')}...`,
     'system',
   )
-  const data = await fs.readFile(file.SERVER_DATA.path, 'utf-8')
-  return Object.values(JSON.parse(data))
+
+  try {
+    const data = await fs.readFile(file.SERVER_DATA.path, 'utf-8')
+    return Object.values(JSON.parse(data))
+  } catch {
+    logging.broadcast(
+      `failed to load ${logging.coloredText(
+        file.SERVER_DATA.name,
+        'highlight',
+      )}, initializing a new one...`,
+      'fail',
+    )
+
+    const newData: ServerObject[] = []
+    await fs.writeFile(file.SERVER_DATA.path, JSON.stringify({}))
+    return newData
+  }
 }
 
 export default { getServerData }
