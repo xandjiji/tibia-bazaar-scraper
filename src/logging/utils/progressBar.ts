b39db5b16a7d67f0a4d8901c52e6e4174ae28f41
xandjiji <xandjiji@gmail.com>
2021-12-21 08:53:13 -0300

    fix(logging): fixing progressBar

--- src/logging/utils/progressBar.ts
+++ src/logging/utils/progressBar.ts
@@ -3,28 +3,26 @@ import { ColorKey } from '../types'
 
 const MAX_WIDTH = 8
 const BLOCKS = [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█']
-const FILL = BLOCKS[BLOCKS.length - 1]
-const [EMPTY] = BLOCKS
-
-const print = (length: number, character: string): string =>
-  Array.from({ length }, () => character).join('')
+const TOTAL_STEPS = MAX_WIDTH * BLOCKS.length
 
 export const progressBar = (
   percentage: number,
   color: ColorKey = 'highlight',
 ): string => {
-  const filled = Math.floor(MAX_WIDTH * percentage)
-  const fill = print(filled, coloredText(FILL, color))
+  const progressCount = Math.floor(TOTAL_STEPS * percentage)
 
-  if (filled === MAX_WIDTH) return `[${fill}]`
+  const bars = []
+  let progressLeft = progressCount
+  while (bars.length < MAX_WIDTH) {
+    for (let i = BLOCKS.length - 1; i >= 0; i--) {
+      const character = BLOCKS[i]
+      if (progressLeft - i >= 0) {
+        progressLeft -= i
+        bars.push(character)
+        break
+      }
+    }
+  }
 
-  const fractionalBlockIndex =
-    Math.round(MAX_WIDTH * BLOCKS.length * percentage) % BLOCKS.length
-
-  const fraction = coloredText(BLOCKS[fractionalBlockIndex], color)
-
-  const unfilled = MAX_WIDTH - filled
-  const rest = print(unfilled - 1, EMPTY)
-
-  return `[${fill}${fraction}${rest}]`
+  return `[${coloredText(bars.join(''), color)}]`
 }
