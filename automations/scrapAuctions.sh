665cba2785e8860f0a98d0ecae36b217684ecc42
xandjiji <xandjiji@gmail.com>
2021-12-29 08:19:26 -0300

    feature(automations): removing unecessary code

--- automations/scrapAuctions.sh
+++ automations/scrapAuctions.sh
@@ -8,23 +8,6 @@ cp ~/tibia-bazaar-scraper/Output/ItemsData.json ~/tibia-bazaar-scraper/Output/st
 cp ~/tibia-bazaar-scraper/Output/HighlightedAuctions.json ~/tibia-bazaar-scraper/Output/static
 sh ~/tibia-bazaar-scraper/Output/static/deployStatic.sh
 
-# remove this after deploy:
-cp ~/tibia-bazaar-scraper/Output/ServerData.json ~/exevo-pan-data/
-cp ~/tibia-bazaar-scraper/Output/ItemsData.json ~/exevo-pan-data/
-cp ~/tibia-bazaar-scraper/Output/HighlightedAuctions.json ~/exevo-pan-data/
-
-# remove this after deploy:
-cd ~/exevo-pan-data/
-git push origin --delete deploy
-git branch -D deploy
-git checkout -b deploy
-
-# remove this after deploy:
-git add .
-git commit -m "`date`"
-git push --set-upstream origin deploy
-git checkout main
-
 cp ~/tibia-bazaar-scraper/Output/CurrentAuctions.json ~/worker-exevopan/src/Data
 cp ~/tibia-bazaar-scraper/Output/ServerData.json ~/worker-exevopan/src/Data
 cp ~/tibia-bazaar-scraper/Output/ItemsData.json ~/worker-exevopan/src/Data
--- automations/updateStatistics.sh
+++ automations/updateStatistics.sh
@@ -5,18 +5,3 @@ yarn update:statistics
 
 cp ~/tibia-bazaar-scraper/Output/HistoryStatistics.json ~/tibia-bazaar-scraper/Output/static
 sh ~/tibia-bazaar-scraper/Output/static/deployStatic.sh
-
-# remove this after deploy:
-cp ~/tibia-bazaar-scraper/Output/overallStatistics.json ~/exevo-pan-history-data/
-
-# remove this after deploy:
-cd ~/exevo-pan-history-data/
-git push origin --delete deploy
-git branch -D deploy
-git checkout -b deploy
-
-# remove this after deploy:
-git add .
-git commit -m "`date`"
-git push --set-upstream origin deploy
-git checkout main
