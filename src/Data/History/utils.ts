f53e3b5893baa4d8ddd14bcc6bfc3e20c0694cd7
xandjiji <xandjiji@gmail.com>
2021-12-29 07:57:11 -0300

    feature(utils): drastically lowering task update frequency for readJsonl() and writeJsonl()

--- src/Data/History/utils.ts
+++ src/Data/History/utils.ts
@@ -18,6 +18,8 @@ export const removeDupedIds = <T extends { id: number }>(array: T[]): T[] => {
   })
 }
 
+const TASK_UPDATE_INTERVAL = 1000
+
 export const readJsonl = async <T>(path: string): Promise<T[]> => {
   const fileStat = await promisedFs.stat(path)
 
@@ -25,14 +27,18 @@ export const readJsonl = async <T>(path: string): Promise<T[]> => {
   const rl = readline.createInterface({ input: fileStream })
   const eta = new TrackETA(fileStat.size)
 
+  const updater = setInterval(
+    () => eta.setCurrentTask(fileStream.bytesRead),
+    TASK_UPDATE_INTERVAL,
+  )
+
   const array: T[] = []
   for await (const line of rl) {
     const object = JSON.parse(line)
     array.push(object)
-
-    eta.setCurrentTask(fileStream.bytesRead)
   }
 
+  clearInterval(updater)
   eta.setCurrentTask(fileStream.bytesRead)
   eta.finish()
 
@@ -41,16 +47,21 @@ export const readJsonl = async <T>(path: string): Promise<T[]> => {
 
 export const writeJsonl = async <T>(path: string, objects: T[]) => {
   const eta = new TrackETA(objects.length)
-
   const fileStream = fs.createWriteStream(path, { flags: 'w' })
 
   return new Promise((resolve, reject) => {
+    let task = 0
+    const PERCENT = Math.round(objects.length / 100)
     for (const object of objects) {
       fileStream.write(`${JSON.stringify(object)}\n`)
-      eta.incTask()
+      task++
+      if (task % PERCENT === 0) {
+        eta.setCurrentTask(task)
+      }
     }
     fileStream.end()
     fileStream.on('finish', () => {
+      eta.setCurrentTask(task)
       eta.finish()
       resolve(true)
     })
